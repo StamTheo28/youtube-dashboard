d5a9e50cd0d0d39b134642aef920315a5ccec61c
StamatisTheocharous@gmail.com <StamatisTheocharous@gmail.com>
2023-07-15 18:32:57 +0100

    Update invalid url message js

--- myDashboard/static/js/dashboard.js
+++ myDashboard/static/js/dashboard.js
@@ -12,13 +12,3 @@ button.addEventListener('click', function() {
 });
 
 
-
-// Error Pop up
-document.addEventListener('DOMContentLoaded', function() {
-    var closeButton = document.getElementById('close');
-    var errorPopup = document.getElementById('error-popup');
-
-    closeButton.addEventListener('click', function() {
-        errorPopup.style.display = 'none';
-    });
-});
