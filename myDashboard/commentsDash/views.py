8570e5833ae949e8003f83bbca5e2e33098c3283
StamatisTheocharous@gmail.com <StamatisTheocharous@gmail.com>
2023-08-11 19:37:17 +0100

    Update error_500 page

--- myDashboard/commentsDash/views.py
+++ myDashboard/commentsDash/views.py
@@ -94,5 +94,5 @@ def error_403(request, exception):
 def error_404(request, exception):
     return render(request, 'errors/404.html', status=404)
 
-def error_500(request, exception):
+def error_500(request):
     return render(request, 'errors/500.html', status=500)
