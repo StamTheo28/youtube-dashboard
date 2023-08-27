9f14c4529044faf27409e01eb77f03ae6a874f3d
StamatisTheocharous@gmail.com <StamatisTheocharous@gmail.com>
2023-08-27 14:53:42 +0100

    Update StaticFiles

--- myDashboard/myDashboard/settings.py
+++ myDashboard/myDashboard/settings.py
@@ -127,11 +127,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Change when deployed on AWS
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 # Default primary key field type
